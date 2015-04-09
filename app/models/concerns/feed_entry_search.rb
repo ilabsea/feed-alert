8a967474157e53b05c46449f168f9f86a7a974c7
Channa Ly <channa.info@gmail.com>
2015-04-09 17:36:22 +0700

    search filtered by alert id

--- app/models/concerns/feed_entry_search.rb
+++ app/models/concerns/feed_entry_search.rb
@@ -1,5 +1,164 @@
 module FeedEntrySearch
+  extend ActiveSupport::Concern
+
+  included do
+    include Elasticsearch::Model
+
+    index_name    "feed_entry-#{Rails.env}"
+    document_type "feed_entry"
+
+    settings index: { number_of_shards: 1 } do
+      mappings do
+        indexes :title, analyzer: 'english', index_options: 'offsets'
+        indexes :summary, analyzer: 'english', index_options: 'offsets'
+        indexes :content, type: 'attachment', fields: { 
+                                                        author: { index: "no"},
+                                                        date: { index: "no"},
+                                                        content: { store: "yes",
+                                                                   type: "string",
+                                                                   term_vector: "with_positions_offsets"
+                                                                }
+                                                      }
+      end
+    end
+
+    after_commit lambda { sync_index('index') }, on: :create
+    after_commit lambda { sync_index('update') }, on: :update
+    after_commit lambda { sync_index('delete') }, on: :destroy
+
+  end
+
+
+  def sync_index(action_type)
+    Rails.logger.debug { "Try to sync index" }
+    if action_type =='delete'
+      IndexerJob.perform_later(action_type, self.class.to_s, self.id)
+    elsif self.has_content?
+      IndexerJob.perform_later(action_type, self.class.to_s, self.id)
+    else
+      Rails.logger.debug { "Index not sync for #{self.id}" }
+    end
+  end
+
   def apply_search
     self.matched = StringSearch.new(self.content).match_keywords?(self.keywords)
   end
+
+  module ClassMethods
+    # Util for creating index
+    def create_index
+      options = {
+        index: self.index_name,
+        body: { settings: self.settings.to_hash,
+                mappings: self.mappings.to_hash
+              }
+      }
+      self.__elasticsearch__.client.indices.create(options)
+    end
+
+    def self.recreate_index
+      self.__elasticsearch__.create_index! force: true
+      self.__elasticsearch__.refresh_index!
+    end
+
+    def as_indexed_json(options={})
+      hash = self.as_json
+      hash["content"] = {
+        "_detect_language": false,
+        "_language": "en",
+        "_indexed_chars": -1 ,
+        # "_content_type": "text/html",
+        "_content": Base64.encode64(self.content)
+      }
+      hash
+    end
+
+    def build_criterias options
+      shoulds = []
+
+      options[:q].each do |alert|
+        keywords = alert[:keywords]
+        alert_id = alert[:id]
+        keywords.each do |keyword|
+          match_phrase = {
+                          query: {
+                              multi_match: {
+                                query: keyword,
+                                type: "phrase",
+                                fields: ['title', 'summary', 'content']
+                              }
+                          },
+                          filter: {
+                            term: {
+                              alert_id: alert_id
+                            }
+                          }
+                       }
+
+          shoulds << { filtered: match_phrase}
+        end
+        
+      end
+
+      dsl = {
+          query: {
+            bool: {
+              should: shoulds
+            }
+          }
+      }
+
+      if options[:from].present? && options[:to].present?
+        filter = {
+                  range: {
+                      created_at: {
+                        gte: options[:from],
+                        lte: options[:to]
+                      }
+                  }
+                }
+
+        dsl[:filter] = filter
+
+        dsl = { query: {
+                         filtered: dsl
+                       }
+              }
+      end
+
+      highlight = {
+        fields: {
+          title: { fragment_size: 200 },
+          summary: { fragment_size: 200 },
+          content: { fragment_size: 200 }
+        }
+      }
+
+      facets = {
+        alert_id_counts: {
+          terms: {
+            field: :alert_id,
+            size:50,
+            all_terms: false
+          }
+        }
+      }
+
+      fields = [:id, :alert_id, :title, :url, :keywords, :created_at]
+      dsl[:highlight] = highlight
+      dsl[:facets] = facets
+      dsl[:fields] = fields
+      dsl[:size] = 50
+      dsl
+    end
+
+    def search(options={})
+      criteria = build_criterias(options)
+      # __elasticsearch__.search(criteria)
+      # use raw version since elasticsearch-model does not support facet query
+      results = self.__elasticsearch__.client.search(index: self.index_name, body: criteria)
+      FeedEntrySearchResultPresenter.new(results)
+    end
+  end
+
 end
