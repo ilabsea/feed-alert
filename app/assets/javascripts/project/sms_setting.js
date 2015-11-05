1e43c15111c4a9bb9f07ff5000fca928c1524d9e
thyda_eng <thyda@instedd.org>
2015-11-05 15:17:55 +0700

    fixes script error for time picker

--- app/assets/javascripts/project/sms_setting.js
+++ app/assets/javascripts/project/sms_setting.js
@@ -2,12 +2,14 @@ $(function(){
   timePicker();
 })
 
-function timePicker (){
+function timePicker(){
   $('#sms-alert-time .time').timepicker({
       'showDuration': true,
       'timeFormat': 'H:i'
   });
 
   var sms_alert_time = document.getElementById('sms-alert-time');
-  var sms_alert_time_pair = new Datepair(sms_alert_time);  
+  if(sms_alert_time != null){
+  	var sms_alert_time_pair = new Datepair(sms_alert_time); 
+  } 
 }
