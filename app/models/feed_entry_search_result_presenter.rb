4f35d66d53e8740c7627a0392b60561a0d9c8db8
thyda_eng <thyda@instedd.org>
2016-01-11 14:35:03 +0700

    updates the new feed_entry result document

--- app/models/feed_entry_search_result_presenter.rb
+++ app/models/feed_entry_search_result_presenter.rb
@@ -1,40 +1,42 @@
 # @response=
-#   {"took"=>12,
-#    "timed_out"=>false,
-#    "_shards"=>{"total"=>5, "successful"=>5, "failed"=>0},
-#    "hits"=>
-#     {"total"=>5,
-#      "max_score"=>0.0150097255,
-#      "hits"=>
-#       [{"_index"=>"feed_entry-development",
-#         "_type"=>"feed_entry",
-#         "_id"=>"553",
-#         "_score"=>0.0150097255,
-#         "_source"=>
-#          {"id"=>553,
-#           "title"=>"Pfizer pledges to ringfence key new drugs in AstraZeneca deal",
-#           "keywords"=>["bussines", "technology", "social", "art", "culture", "gloabl", "news", "deal", "attempt", "sweet"],
-#           "created_at"=>"2015-12-23T10:12:01.000+07:00",
-#           "url"=>
-#            "http://reuters.us.feedsportal.com/c/35217/f/654216/s/3a68b59c/sc/7/l/0L0Sreuters0N0Carticle0C20A140C0A50C140Cus0Eastrazeneca0Epfizer0EidUSBREA3R0AH520A140A5140DfeedType0FRSS0GfeedName0FglobalMarketsNews/story01.htm",
-#           "alert_id"=>15},
-#         "highlight"=>{"title"=>["Pfizer pledges to ringfence key new drugs in AstraZeneca <em class='highlight'>deal</em>"]}},
-#        {"_index"=>"feed_entry-development",
-#         "_type"=>"feed_entry",
-#         "_id"=>"555",
-#         "_score"=>0.013266775,
-#         "_source"=>
-#          {"id"=>555,
-#           "title"=>"Pfizer defends 'powerhouse' Astra deal as CEO braces for grilling",
-#           "keywords"=>["bussines", "technology", "social", "art", "culture", "gloabl", "news", "deal", "attempt", "sweet"],
-#           "created_at"=>"2015-12-23T10:12:06.000+07:00",
-#           "url"=>
-#            "http://reuters.us.feedsportal.com/c/35217/f/654216/s/3a50290e/sc/2/l/0L0Sreuters0N0Carticle0C20A140C0A50C120Cus0Eastrazeneca0Epfizer0EidUSBREA3R0AH520A140A5120DfeedType0FRSS0GfeedName0FglobalMarketsNews/story01.htm",
-#           "alert_id"=>15},
-#         "highlight"=>{"title"=>["Pfizer defends 'powerhouse' Astra <em class='highlight'>deal</em> as CEO braces for grilling"]}}
-#        ]
-#      }
-#    }
+  # {"took"=>101,
+  #  "timed_out"=>false,
+  #  "_shards"=>{"total"=>5, "successful"=>5, "failed"=>0},
+  #  "hits"=>
+  #   {"total"=>20,
+  #    "max_score"=>0.116118275,
+  #    "hits"=>
+  #     [{"_index"=>"feed_entries",
+  #       "_type"=>"feed_entry",
+  #       "_id"=>"AVIuhR2jd6Jj8nutJGrS",
+  #       "_score"=>0.116118275,
+  #       "_source"=>
+  #        {"title"=>"Frutarom invests in algae startup for food, cosmetic products",
+  #         "keywords"=>["drug", "emergency", "startup", "environment"],
+  #         "created_at"=>"2016-01-11T02:30:21.852Z",
+  #         "url"=>
+  #          "http://reuters.us.feedsportal.com/c/35217/f/654220/s/4ca2a6df/sc/28/l/0L0Sreuters0N0Carticle0Cus0Efrutarom0Einds0Ealgae0EidUSKBN0AUI0AP520A160A10A40DfeedType0FRSS0GfeedName0FscienceNews/story01.htm",
+  #         "alert_id"=>31},
+  #       "highlight"=>
+  #        {"content"=>
+  #          ["                          \n\t\n                                                       <em class='highlight'>Environment</em>\n                                                    \n\t\n                             ",
+  #           "         \n                Science, \n                <em class='highlight'>Environment</em>\n                \n        \n\n    Frutarom invests in algae <em class='highlight'>startup</em> for food, cosmetic products\n\n                \n     "],
+  #         "title"=>["Frutarom invests in algae <em class='highlight'>startup</em> for food, cosmetic products"]}},
+  #      {"_index"=>"feed_entries",
+  #       "_type"=>"feed_entry",
+  #       "_id"=>"AVIuhJrxd6Jj8nutJGrF",
+  #       "_score"=>0.01754997,
+  #       "_source"=>
+  #        {"title"=>"Illumina, partners make $100 million bet to detect cancer via blood test",
+  #         "keywords"=>["drug", "emergency", "startup", "environment"],
+  #         "created_at"=>"2016-01-11T02:29:48.391Z",
+  #         "url"=>
+  #          "http://reuters.us.feedsportal.com/c/35217/f/654220/s/4cc53781/sc/28/l/0L0Sreuters0N0Carticle0Cus0Eusa0Ehealthcare0Eillumina0EidUSKCN0AUO0AW920A160A1110DfeedType0FRSS0GfeedName0FscienceNews/story01.htm",
+  #         "alert_id"=>31},
+  #       "highlight"=>
+  #        {"content"=>
+  #          ["                          \n\t\n                                                       <em class='highlight'>Environment</em>\n                                                    \n\t\n                             ",
+  #           "diagnosed with cancer to see how they are responding to treatment or to check for mutations or <em class='highlight'>drug</em> resistance.\n\n\n        \n        Critics said there is not enough evidence yet that a blood"]}},
 
 class FeedEntrySearchResultPresenter
   attr_accessor :response
