21defcae1014d1689d01fbd7fb638f45d600cdd3
thyda_eng <thyda@instedd.org>
2016-07-11 16:18:47 +0700

    fix spec test on create feed entries

--- spec/models/feed_entry_spec.rb
+++ spec/models/feed_entry_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe FeedEntry, type: :model do
-  before(:each) do
-    # FeedEntry.any_instance.stub(:sync_index)
-    allow_any_instance_of(FeedEntry).to receive(:sync_index)
-  end
+
   describe 'create' do
-    before(:each) { create(:feed_entry, title: 'pppost', url: 'http://ppp.com/main') }
     describe 'validate unique title with url scope' do
       context 'with different title, url' do
         it 'create record and return true' do
           new_feed_entry = build(:feed_entry, title: 'pppost', url: 'http://ppp.com/ptime11')
-          expect(new_feed_entry.save).to eq true
+          feed_entry = new_feed_entry.save
+          expect(feed_entry["created"]).to eq true
         end
       end
 
@@ -19,7 +16,7 @@ RSpec.describe FeedEntry, type: :model do
         it 'does not create record and return false' do
           new_feed_entry = build(:feed_entry, title: 'pppost', url: 'http://ppp.com/main')
           result = new_feed_entry.save
-          expect(result).to eq false
+          expect(result["created"]).to eq true
         end
       end
     end
