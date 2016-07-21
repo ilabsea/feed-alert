01df258c3e436568f2b63a76e70a9af1ae8eaf06
thyda_eng <thyda@instedd.org>
2016-07-21 10:05:32 +0700

    add elasticsearch mapper-attachment plugin to elasticsearch

--- spec/models/feed_entry_spec.rb
+++ spec/models/feed_entry_spec.rb
@@ -5,21 +5,23 @@ RSpec.describe FeedEntry, type: :model do
   describe 'create' do
     describe 'validate unique title with url scope' do
       context 'with different title, url' do
+        let(:new_feed_entry) { build(:feed_entry, title: 'pppost', url: 'http://ppp.com/ptime11') }
+
         it 'create record and return true' do
-          new_feed_entry = build(:feed_entry, title: 'pppost', url: 'http://ppp.com/ptime11')
           feed_entry = new_feed_entry.save
           expect(feed_entry["created"]).to eq true
         end
       end
 
       context 'duplicate title with url scope' do
+        let(:new_feed_entry) { build(:feed_entry, title: 'pppost', url: 'http://ppp.com/main') }
+
         it 'does not create record and return false' do
-          new_feed_entry = build(:feed_entry, title: 'pppost', url: 'http://ppp.com/main')
           result = new_feed_entry.save
           expect(result["created"]).to eq true
         end
       end
     end
-
   end
+
 end
