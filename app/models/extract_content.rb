d6c891cda0ec33aa62f99665f8fe1c56339a13c9
thyda_eng <thyda@instedd.org>
2016-07-22 10:22:48 +0700

    rename method run to fetch in extract content and fix yml example

--- app/models/extract_content.rb
+++ app/models/extract_content.rb
@@ -3,10 +3,9 @@ class ExtractContent
 
   def self.instance
     @@instance ||= ExtractContent.new
-    @@instance
   end
 
-  def run(url)
+  def fetch(url)
     source = FetchPage.instance.run(url)
     Readability::Document.new(source).content
   end
