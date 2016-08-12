a097ae6df5a08f8408148686f2111a399bdddf87
thyda_eng <thyda@instedd.org>
2016-08-12 15:43:26 +0700

    fix spec test on group message

--- spec/models/group_message_spec.rb
+++ spec/models/group_message_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe GroupMessage, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:receiver_groups)}
+    it { should validate_presence_of(:receiver_groups).with_message("Receiver's group cannot be blank")}
   end
 end
