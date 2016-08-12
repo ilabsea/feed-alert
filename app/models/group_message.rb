3f0c95b4f756be1d8a131b1eaf63b2d49593341c
thyda_eng <thyda@instedd.org>
2016-08-12 13:48:35 +0700

    finish fix group message sending fail

--- app/models/group_message.rb
+++ app/models/group_message.rb
@@ -3,14 +3,13 @@ class GroupMessage < ActiveRecord::Base
 
   serialize :receiver_groups
 
-  validates :receiver_groups, presence: true
+  validates_presence_of :receiver_groups, :message => "Receiver's group cannot be blank"
 
   def send_ao!
     if sms_alert
       active_channels = Channels::Accessible::UserChannelAccessible.new(self.user).list
       if active_channels.empty?
         errors.add(:base, "No sms channel found, please make sure you have the accessible channel before sending the sms")
-        save!
         raise Errors::UnknownChannelExpcetion.new(self)
       end
     end
