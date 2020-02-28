834c9a286521a65dc4a3e883d8c2967e45ffa92b
Borey Lim <lim.borey@gmail.com>
2020-02-28 17:29:39 +0700

    update digest mail title

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,7 +1,7 @@
 class AlertMailer < ApplicationMailer
   def notify_matched(email, snapshots)
     @snapshots = snapshots
-    roadie_mail(to: email, subject: "Feed alert digest email")
+    roadie_mail(to: email, subject: "Hourly Digest - Keywords Matched")
   end
 
   def notify_group_message(group_message, emails_to)
