54faa0954c9ae837e7ef55e8756ced301131a342
Borey Lim <lim.borey@gmail.com>
2020-03-05 16:44:17 +0700

    add keyword set to alert

--- app/controllers/alert_keywords_controller.rb
+++ app/controllers/alert_keywords_controller.rb
@@ -1,13 +1,9 @@
 class AlertKeywordsController < ApplicationController
   def create
     @alert = Alert.find(params[:alert_id])
-    keyword = Keyword.find_or_create_by(name: params[:keyword].strip)
-    if !keyword.persisted?
-      render text: keyword.errors.full_messages.first, status: 422
-      return
-    end
+    keyword = current_user.keyword_sets.find_by(name: params[:keyword].strip)
 
-    alert_keyword = @alert.alert_keywords.build(keyword_id: keyword.id)
+    alert_keyword = AlertKeywordSet.new(keyword_set: keyword, alert: @alert)
 
     if alert_keyword.save
       render layout: false
@@ -17,7 +13,7 @@ class AlertKeywordsController < ApplicationController
   end
 
   def destroy
-    alert_keyword = AlertKeyword.find(params[:id])
+    alert_keyword = AlertKeywordSet.find(params[:id])
     alert_keyword.destroy
     head :ok
   end
--- app/models/alert_keyword_set.rb
+++ app/models/alert_keyword_set.rb
@@ -1,4 +1,7 @@
 class AlertKeywordSet < ActiveRecord::Base
-  belongs_to :alert, counter_cache: true, touch: true
+  belongs_to :alert, touch: true
   belongs_to :keyword_set
+
+  validates :alert_id, presence: true, uniqueness: { scope: :keyword_set_id }
+  validates :keyword_set_id, presence: true
 end
